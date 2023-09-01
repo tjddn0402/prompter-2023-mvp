a0785877f819835c7f10b25a023af6e653c34ef0
JY Suh <dddsaty09@naver.com>
2023-09-01 15:13:36 +0900

    feat: Adding law API call file and some samples
    Call API and save it as json files

---
+++ law_data/public_data_api_call.py
@@ -0,0 +1,44 @@
+"""
+국가법령정보센터 API에서 불러와서 데이터프레임으로 저장하는 코드
+API URL : https://www.data.go.kr/data/15000115/openapi.do
+required libraries :
+  - requests
+  - json
+  - os
+  - xml_to_dict
+"""
+import requests
+import json
+import os
+
+from xml_to_dict import XMLtoDict
+def law_public_api_call(query='10', numOfRows='10', pageNo='1'):
+    with open("service_key.txt", encoding="utf-8") as f:
+        key = str(f.readline())
+
+    url = 'http://apis.data.go.kr/1170000/law/lawSearchList.do'
+
+    params ={'serviceKey' : key, 'target' : 'law', 'query' : query, 'numOfRows' : numOfRows, 'pageNo' : pageNo }
+
+    response = requests.get(url, params=params)
+    encoding_type = str(response.encoding)
+    contents = response.content.decode(encoding_type)
+
+    XD = XMLtoDict()
+    contents_to_dict = XD.parse(contents)
+
+    path = "./law_json_files/"
+    os.makedirs(path, exist_ok=True)
+
+    for i in range(len(contents_to_dict["LawSearch"]["law"])):
+        name = contents_to_dict["LawSearch"]["law"][i]["법령명한글"]
+        file = contents_to_dict["LawSearch"]["law"][i]
+        with open(f"{path}{name}.json", "w", encoding="utf-8") as f:
+            json.dump(file, f, indent=4)
+
+# all params should be "str"
+law_public_api_call(
+    query= '10',
+    numOfRows= '10',
+    pageNo= '1'
+)
